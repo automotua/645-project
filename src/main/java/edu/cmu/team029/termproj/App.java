7e84a94e475c2a1a0b08f61b94bdb4e119d5bcb4
automotua <adam.junqiang.li@gmail.com>
2016-04-23 00:27:27 -0400

    first commit for project files

---
+++ src/main/java/edu/cmu/team029/termproj/App.java
@@ -0,0 +1,13 @@
+package edu.cmu.team029.termproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
